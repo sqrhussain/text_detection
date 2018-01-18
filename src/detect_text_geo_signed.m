954be2254d0b7783c95fc3a6bdcdb3b8006d15aa
Hussain Hussain <hussain@student.tugraz.at>
2018-01-18 18:39:18 +0100

    Improved detection

--- src/detect_text_geo_signed.m
+++ src/detect_text_geo_signed.m
@@ -6,6 +6,8 @@ function predict = detect_text_geo_signed(I,model,sign)
 sigma = 1;
 width = 100;
 stdthresh = 10;
+smallmin = 5;
+smallmax = 10;
 im = rgb2gray(I);
 %im = imcrop(im);
 lap = sign * fspecial('log',[1,width],sigma);
@@ -16,8 +18,8 @@ fim = (fim1+fim2)*.5;
 [H,W] = size(im);
 visited = false(size(im));
 predict = zeros(size(im));
-
-for wy = 60:-12:24
+hbw = im2bw(fim,graythresh(fim));
+for wy = 60:-12:12
     wx = 3*wy;
     xstride = wx/3;
     ystride = wy/3;
@@ -27,7 +29,7 @@ for wy = 60:-12:24
         for x = 1:xstride:W-wx
     
             c = imcrop(fim,[x,y,wx,wy]);
-            if std2(c) < 10
+            if std2(c) < stdthresh
                 continue
             end
 %             imshow(c);
@@ -35,14 +37,18 @@ for wy = 60:-12:24
 %             pause;
 
             %c = im2bw(c);
-    %         imshow(im);
-    %         hold on
-    %         rectangle('Position', [x,y,wx,wy],...
-    %         'EdgeColor','r', 'LineWidth', 1)
-    %         hold off
-    %         pause(0.001);
-            %c = imadjust(c);
-            bw = im2bw(c,graythresh(c));
+%             subplot(1,2,1)
+%             imshow(im);
+%             hold on
+%             rectangle('Position', [x,y,wx,wy],...
+%             'EdgeColor','r', 'LineWidth', 1)
+%             hold off
+%             pause(0.001);
+            
+%             bw = im2bw(c,graythresh(c));
+            bw = imcrop(hbw,[x,y,wx,wy]);
+%             subplot(1,2,2)
+%             imshow(bw);
             [labeledImage,numlabels] = bwlabel(bw);
             measurements = regionprops(labeledImage,...
                 'BoundingBox');
@@ -57,9 +63,9 @@ for wy = 60:-12:24
 %                 imshow(region);
 %                 title(num2str(numlabels))
 %                 pause(0.01);
-                %region = imcrop(region, bounds);
-    %             size(region)
-    %             size(visited(y:y+wy,x:x+wx))
+%                 region = imcrop(region, bounds);
+%                 size(region)
+%                 size(visited(y:y+wy,x:x+wx))
                 if sum(sum(visited(y:y+wy,x:x+wx) & region)) == sum(region(:))
                     continue;
                 end
@@ -69,14 +75,20 @@ for wy = 60:-12:24
                 if std2(gc) < stdthresh % too smooth
                     continue;
                 end 
-                if min(bounds(3:4)) < 10 || bounds(3)*bounds(4) < 100 % too small
+                if min(bounds(3:4)) < smallmin ||...
+                        max(bounds(3:4)) < smallmax ||...
+                        bounds(3)*bounds(4) < smallmin*smallmin % too small
+%                     imshow(region);
+%                     pause
                     continue
                 end
                 features = geometric_features(imcrop(bw, bounds));
                 p = model(features);
                 if p>0.5
                     predict(y:y+wy,x:x+wx) = predict(y:y+wy,x:x+wx) | region;
-
+%                 else
+%                     imshow(imcrop(bw, bounds));
+%                     pause
                 end
 %                 imshow(imcrop(region, bounds));
 %                 title('region');
@@ -103,9 +115,15 @@ for wy = 60:-12:24
         features = geometric_features(imcrop(region, bounds));
         p = model(features);
         if p<0.5
+%             imshow(region)
+%             title('p < 0.5');
+%             pause
             predict = predict & (~region);
         end
-        if min(bounds(3:4)) < 10 % too small
+        if min(bounds(3:4)) < smallmin || max(bounds(3:4)) < smallmax  % too small
+            imshow(region)
+            title('min(bounds(3:4)) < smallx');
+            pause
             predict = predict & (~region);
         end
         if bounds(3) < bounds(4)
@@ -125,6 +143,10 @@ for wy = 60:-12:24
 %             hold off
 %             pause(0.01)
         if sum(cc(:)) / (bounds(3)*bounds(4)) < 0.15 % no enough white pixels, could be a thin noisy blob
+            
+%             imshow(cc)
+%             title('sum(cc(:)) / (bounds(3)*bounds(4)) < 0.15');
+%             pause
             predict = predict & (~region);
         end
             
@@ -164,6 +186,8 @@ for wy = 60:-12:24
 end
 av = fspecial('average',[1,36]);
 f = imfilter(im2double(predict),av);
+imshow(f)
+pause
 bw = im2bw(f,graythresh(f));
 predict = bw;
 [labeledImage,numlabels] = bwlabel(predict);
@@ -175,7 +199,7 @@ for i = 1 : numlabels
     region = region > 0;
     bounds = measurements(i).BoundingBox;
     gc = imcrop(fim,bounds);
-    if min(bounds(3:4)) < 5 % too small
+    if min(bounds(3:4)) < smallmin ||max(bounds(3:4)) < smallmax  % too small
         predict = predict & (~region);
     end
     if std2(gc) < stdthresh % too smooth
